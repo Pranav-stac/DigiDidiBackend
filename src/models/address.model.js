842924c56b10ce357c4207c454bbe158e4a6ef5e
Durgesh-05 <durgeshkumar.d@somaiya.edu>
2024-12-20 21:09:50 +0530

    Added address, notification & review table

---
+++ src/models/address.model.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const addressSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    street: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    state: {
+      type: String,
+      required: true,
+    },
+    zipcode: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    isPrimary: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Address = mongoose.model('Address', addressSchema);
+export default Address;
---
+++ src/models/review.model.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const reviewSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: [1, 'Minimum rating is 1'],
+      max: [5, 'Rating Cannot exceed 5'],
+    },
+    comment: {
+      type: String,
+    },
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+export default Review;
